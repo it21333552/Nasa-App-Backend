e9ddda816558e5170930bc75729fb4e2e4a1f33b
Ishani Abeysinghe <99570856+it21333552@users.noreply.github.com>
2024-05-11 10:33:10 +0530

    Update CorsConfig.java

--- nasa/src/main/java/com/example/nasa/config/CorsConfig.java
+++ nasa/src/main/java/com/example/nasa/config/CorsConfig.java
@@ -6,12 +6,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("/api/*") // Add your frontend URL here
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedHeaders("*");
     }
 }
