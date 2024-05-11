f14dd1bfdb8e5cefa91eb93f26b24217ff476a5c
Ishani Abeysinghe <99570856+it21333552@users.noreply.github.com>
2024-05-11 10:31:22 +0530

    Update CorsConfig.java

--- nasa/src/main/java/com/example/nasa/config/CorsConfig.java
+++ nasa/src/main/java/com/example/nasa/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // Add your frontend URL here
+                .allowedOrigins("/api/*") // Add your frontend URL here
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
